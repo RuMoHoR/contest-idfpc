d63eae73ee91fe598729b5dce34a08c7921d6ad1
Anton Gogin <git@rumohor.com>
2013-02-17 09:17:57 +0400

    VVVM div zero msg, cleanup
    Signed-off-by: Anton Gogin <git@rumohor.com>

--- src/vvvm.c
+++ src/vvvm.c
@@ -31,15 +31,9 @@ vvvm_get(
 	int	ret;
 
 	if ( ( vm->ip + offset ) >= VVVM_SIZE ) {
-//		printf( "GET: Bad IP+Offs=%u+%d=%u\n",
-//			vm->ip, offset, vm->ip + offset );
 		op->word = 0;
 		ret = 0;
 	} else { /* addr ok */
-//		printf( "GET: IP+Offs=%u+%d=[%u]=%08X(%d)\n",
-//			vm->ip, offset, vm->ip + offset,
-//			vm->data[ vm->ip + offset ].word,
-//			vm->data[ vm->ip + offset ].offset );
 		*op = vm->data[ vm->ip + offset ];
 		ret = 1;
 	} /* addr ok? */
@@ -57,13 +51,8 @@ vvvm_put(
 	int	ret;
 
 	if ( ( vm->ip + offset ) >= VVVM_SIZE ) {
-//		printf( "PUT: Bad IP+Offs=%u+%d=%u\n",
-//			vm->ip, offset, vm->ip + offset );
 		ret = 0;
 	} else { /* addr ok */
-//		printf( "PUT: IP+Offs=%u+%d=[%u]=%08X(%d)\n",
-//			vm->ip, offset, vm->ip + offset,
-//			op->word, op->offset );
 		vm->data[ vm->ip + offset ] = *op;
 		ret = 1;
 	} /* addr ok? */
@@ -111,7 +100,11 @@ vvvm_binop_div(
 	const unsigned int b,
 	const unsigned int c )
 {
-	if ( !c ) return 0;
+	if ( !c ) {
+	printf( "DIV zero\n" );
+
+		return 0;
+	}
 
 	*a = b / c;
 	return 1;
@@ -183,19 +176,10 @@ vvvm_cmd_binop(
 	g2 = vvvm_get( vm, 2, &op2 );
 	g3 = vvvm_get( vm, 3, &y );
 
-//	printf( "BIN: ofs %10d(%d) %10d(%d) %10d(%d)\n",
-//		op1.offset, g1,
-//		op2.offset, g2,
-//		y.offset, g3 );
-
 	if ( g1 && g2 && g3 ) {
 		g2 = vvvm_get( vm, op2.offset, &x );
-//	printf( "BIN: val %10d(%d)\n",
-//		x.offset, g2 );
-		if ( g2 && g3 ) {
+		if ( g2 ) {
 			f = func( &( z.word ), x.word, y.word );
-//	printf( "BIN: fun %10d(%d)\n",
-//		z.offset, f );
 			if ( f ) {
 				ret = vvvm_put( vm, op1.word, &z );
 			} /* func ok? */
@@ -219,11 +203,6 @@ vvvm_cmd_op_not(
 	ret = 0;
 	g1 = vvvm_get( vm, 1, &op1 );
 	g2 = vvvm_get( vm, 2, &x );
-
-//	printf( "BIN: ofs %10d(%d) %10d(%d) %10d(%d)\n",
-//		op1.offset, g1,
-//		op2.offset, g2,
-//		y.offset, g3 );
 
 	if ( g1 && g2 ) {
 		y.word = ~x.word;
@@ -250,15 +229,10 @@ vvvm_cmd_op_jmp(
 	g2 = vvvm_get( vm, 2, &x );
 	g3 = vvvm_get( vm, 3, &y );
 
-//	printf( "BIN: ofs %10d(%d) %10d(%d) %10d(%d)\n",
-//		op1.offset, g1,
-//		op2.offset, g2,
-//		y.offset, g3 );
-
 	if ( g1 && g2 && g3 ) {
 		if ( x.offset < y.offset ) {
 			*jadd = jmp.offset;
-		} /* operands ok? */
+		} /* compare? */
 		ret = 1;
 	} /* offsets ok? */
 
@@ -277,13 +251,7 @@ vvvm_cmd_op_print(
 	ret = 0;
 	g1 = vvvm_get( vm, 1, &x );
 
-//	printf( "BIN: ofs %10d(%d) %10d(%d) %10d(%d)\n",
-//		op1.offset, g1,
-//		op2.offset, g2,
-//		y.offset, g3 );
-
 	if ( g1 ) {
-//		printf( "PRN[%c]\n", ( x.word & 0xFF ) );
 		printf( "%c", ( x.word & 0xFF ) );
 		ret = 1;
 	} /* offsets ok? */
@@ -306,47 +274,36 @@ vvvm_cmd(
 
 	switch ( cmd ) {
 	case 1:
-//		printf( "CMD: +\n" );
 		ret = vvvm_cmd_binop( vm, vvvm_binop_plus );
 		break;
 	case 2:
-//		printf( "CMD: -\n" );
 		ret = vvvm_cmd_binop( vm, vvvm_binop_minus );
 		break;
 	case 3:
-//		printf( "CMD: *\n" );
 		ret = vvvm_cmd_binop( vm, vvvm_binop_mul );
 		break;
 	case 4:
-//		printf( "CMD: /\n" );
 		ret = vvvm_cmd_binop( vm, vvvm_binop_div );
 		break;
 	case 5:
-//		printf( "CMD: &\n" );
 		ret = vvvm_cmd_binop( vm, vvvm_binop_and );
 		break;
 	case 6:
-//		printf( "CMD: |\n" );
 		ret = vvvm_cmd_binop( vm, vvvm_binop_or );
 		break;
 	case 7:
-//		printf( "CMD: <\n" );
 		ret = vvvm_cmd_binop( vm, vvvm_binop_shl );
 		break;
 	case 8:
-//		printf( "CMD: >\n" );
 		ret = vvvm_cmd_binop( vm, vvvm_binop_shr );
 		break;
 	case 9:
-//		printf( "CMD: ~\n" );
 		ret = vvvm_cmd_op_not( vm );
 		break;
 	case 10:
-//		printf( "CMD: J\n" );
 		ret = vvvm_cmd_op_jmp( vm, &jadd );
 		break;
 	case 11:
-//		printf( "CMD: P\n" );
 		ret = vvvm_cmd_op_print( vm );
 		break;
 	default:
@@ -371,11 +328,7 @@ vvvm_step(
 	ret = 0;
 	r = vvvm_get( vm, 0, &op );
 	if ( r ) {
-//		printf( "IP=%u C=%08X cmd=%04X offs=%d\n",
-//			vm->ip, op.word, op.cmd.code, op.cmd.jump );
 		ret = vvvm_cmd( vm, op.cmd.code, op.cmd.jump );
-//		vm->ip += op.cmd.jump;
-//		ret = 1;
 	} /* addr ok?*/
 
 	return ret;
@@ -391,11 +344,9 @@ vvvm_run(
 
 	steps = 0;
 	do {
-//	printf( "\nVVVM step: %u\n", steps );
 		r = vvvm_step( vm );
 		steps++;
-//	printf( "\n" );
-	} while ( r /*&& ( steps < 200000000 )*/ );
+	} while ( r && ( steps < 2000000000 ) );
 
 	printf( "VVVM steps: %u\n", steps );
 }
@@ -407,7 +358,6 @@ contest_vvvm(
 	struct vvvm_t	vm;
 
 	vm.ip = 32;
-	printf( "S=%zd\n", sizeof( union vvvm_opcode_t ) );
 	vm.mem = VVVM_SIZE * sizeof( union vvvm_opcode_t );
 	vm.data = malloc( vm.mem );
 	if ( vm.data ) {
