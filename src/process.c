36ffb62b6c9494e3554722ae29b37128e2fa83f0
Anton Gogin <git@rumohor.com>
2013-02-17 11:08:06 +0400

    tiny algo parser
    Signed-off-by: Anton Gogin <git@rumohor.com>

--- src/process.c
+++ src/process.c
@@ -10,6 +10,8 @@
 struct contest_producer_t {
 	int	value;
 	int	next;
+	int	A;
+	int	B;
 	int	parent[ 4 ];
 	int	parent_num;
 };
@@ -69,11 +71,202 @@ produce_data_init( void )
 	prod = malloc( PRODUCER_SIZE * sizeof( struct contest_producer_t ) );
 	if ( prod ) {
 		for ( i = 0; i < PRODUCER_SIZE; i++ ) {
-			prod[ i ].value = i *24;
-			prod[ i ].next = ( i * 73 ) % 256;
+			prod[ i ].value = 0;
+			prod[ i ].next = rand() % 256;
+			prod[ i ].A = 0;
+			prod[ i ].B = 0;
+			prod[ i ].parent_num = 0;
+			prod[ i ].parent[ 0 ] = -1;
+			prod[ i ].parent[ 1 ] = -1;
+			prod[ i ].parent[ 2 ] = -1;
+			prod[ i ].parent[ 3 ] = -1;
 		} /* all items */
 	} /* alloc ok? */
 	return prod;
+}
+
+#define	PRODUCER_LINE_SIZE	160
+
+static
+int
+produce_data_read_line_pid(
+	struct contest_producer_t * const prod,
+	const char * const line )
+{
+	int	n;
+	int	pid;
+	int	ret;
+
+	ret = 0;
+	n = sscanf( line, "Process %d\n", &pid );
+	if ( n > 0 ) {
+		printf( "PID: %d [%d]\n", n, pid );
+		ret = 1;
+	} /* pid? */
+	return ret;
+}
+
+static
+int
+produce_data_read_line_send(
+	struct contest_producer_t * const prod,
+	const char * const line )
+{
+	int	n;
+	int	pid;
+	int	ret;
+
+	ret = 0;
+	n = sscanf( line, "  send Value to process %d,\n", &pid );
+	if ( n > 0 ) {
+		printf( "SEND: %d [%d]\n", n, pid );
+		ret = 1;
+	} /* pid? */
+	return ret;
+}
+
+static
+int
+produce_data_read_line_value1(
+	struct contest_producer_t * const prod,
+	const char * const line )
+{
+	int	n;
+	int	vala, valb;
+	char	c1;
+	int	ret;
+
+	ret = 0;
+	n = sscanf( line, "  Value <- %d * %c / 64 +%d\n", &vala, &c1, &valb );
+	if ( n == 3 ) {
+		printf( "VAL: %d [%d][%d]\n", n, vala, valb );
+		ret = 1;
+	} /* pid? */
+	return ret;
+}
+
+static
+int
+produce_data_read_line_value2(
+	struct contest_producer_t * const prod,
+	const char * const line )
+{
+	int	n;
+	int	vala, valb;
+	char	c1;
+	int	ret;
+
+	ret = 0;
+	n = sscanf( line, "  Value <- %d * %c / 64 %d\n", &vala, &c1, &valb );
+	if ( n == 3 ) {
+		printf( "VAL: %d [%d][%d]\n", n, vala, valb );
+		ret = 1;
+	} /* pid? */
+	return ret;
+}
+
+static
+int
+produce_data_read_line(
+	struct contest_producer_t * const prod,
+	const char * const line )
+{
+	int	ret;
+	int	r;
+
+	ret = 0;
+
+//	printf( "Line: [%s]\n", line );
+
+	r = produce_data_read_line_pid( prod, line );
+	if ( r ) {
+		ret = 1;
+	} /* pid?*/
+
+	r = produce_data_read_line_send( prod, line );
+	if ( r ) {
+		ret = 1;
+	} /* pid?*/
+
+	r = produce_data_read_line_value1( prod, line );
+	if ( r ) {
+		ret = 1;
+	} /* pid?*/
+
+	r = produce_data_read_line_value2( prod, line );
+	if ( r ) {
+		ret = 1;
+	} /* pid?*/
+
+	if ( !ret ) {
+//		printf( "Line: [%s]\n", line );
+	}
+
+	ret = 1;
+
+	return ret;
+}
+
+static
+int
+produce_data_read(
+	struct contest_producer_t * const prod,
+	const char * const fname )
+{
+	FILE	*falgo;
+	char	line[ PRODUCER_LINE_SIZE + 1 ];
+	int	lines;
+	char	*r;
+	int	p;
+	int	ret;
+
+	ret = 0;
+	falgo = fopen( fname, "r" );
+	if ( !falgo ) {
+		printf( "Can't open '%s', no algo!\n", fname );
+		ret = 0;
+	} else { /* open ok */
+		lines = 0;
+		do {
+			r = fgets( line, PRODUCER_LINE_SIZE, falgo );
+			if ( r ) {
+				p = produce_data_read_line( prod, line );
+				lines++;
+			} /* have line? */
+		} while ( r && p && ( lines < 28 ) );
+		ret = 1;
+	} /* open oK? */
+
+	return ret;
+}
+
+static
+int
+produce_data_validate(
+	struct contest_producer_t * const prod )
+{
+	int	i;
+	int	parent;
+	int	ret;
+	int	errors;
+
+	ret = 1;
+	errors = 0;
+	for ( i = 0; ( i < PRODUCER_SIZE) && ( errors < 3 ); i++ ) {
+		if ( prod[ i ].parent_num != 4 ) {
+			printf( "CP: num_parent = %d\n", prod[ i ].parent_num );
+			ret = 0;
+			errors++;
+		} /* bad num parent */
+		for ( parent = 0; parent < 4; parent++ ) {
+			if ( prod[ i ].parent[ parent ] == -1 ) {
+				printf( "CP: parent[%d] missing\n", parent );
+				ret = 0;
+				errors++;
+			} /* bad parent */
+		} /* all parents */
+	} /* all items */
+	return ret;
 }
 
 static
@@ -110,7 +303,7 @@ produce_image_fill(
 	int	color;
 
 	for ( i = 0; i < PRODUCER_SIZE; i++ ) {
-		color = prod[ i ].value;
+		color = prod[ i ].next;
 		bmp->result_color[ i ].A = color;
 		bmp->result_color[ i ].B = color;
 		bmp->result_color[ i ].C = color;
@@ -125,25 +318,30 @@ contest_produce(
 	struct contest_data_t	*bmp;
 	char	fname[ PATH_MAX + 1 ];
 	int	step;
+	int	r;
 
 	prod = produce_data_init();
 
 	if ( prod ) {
-		bmp = produce_image_create();
-		produce_image_fill( prod, bmp );
-		bmpread_save( bmp, fdir, "50_initial", bmp->result_color );
 
-		for ( step = 1; step <= 7; step++ ) {
-			produce_copy( prod );
-			produce_calculate( prod );
+		produce_data_read( prod, "algo" );
+		r = produce_data_validate( prod );
+		if ( r ) {
+			bmp = produce_image_create();
 			produce_image_fill( prod, bmp );
-			snprintf(
-				fname, PATH_MAX,
-				"5%d_step%d",
-				step, step );
-			bmpread_save( bmp, fdir, fname, bmp->result_color );
-		} /* 7 steps */
+			bmpread_save( bmp, fdir, "50_initial", bmp->result_color );
 
-		bmpread_free( bmp );
+			for ( step = 1; step <= 7; step++ ) {
+				produce_copy( prod );
+				produce_calculate( prod );
+				produce_image_fill( prod, bmp );
+				snprintf(
+					fname, PATH_MAX,
+					"5%d_step%d",
+					step, step );
+				bmpread_save( bmp, fdir, fname, bmp->result_color );
+			} /* 7 steps */
+			bmpread_free( bmp );
+		} /* check ok? */
 	} /* alloc ok? */
 }
